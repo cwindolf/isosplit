48ae152a980d4a7cb0863c24f9341e2fe2f128e2
Charlie Windolf <charliexwindolf@gmail.com>
2021-06-14 14:38:58 -0400

    clean up up_down logic

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ a numpy implementation of J. Magland and A. Barnett, [Unimodal clustering using 
 
 incomplete, unofficial, and in no way endorsed by those authors. see the [official implementation](https://github.com/flatironinstitute/isosplit5). this code is (or, will be) a direct translation to Python of the MATLAB implementation there.
 
-depends on `scikit-learn` in addition to numpy for the isotonic regression.
-
 ### demo
 
 run `pip install -e .` in this directory, and then check out the demo notebook in `demo/`.
--- isosplit/up_down.py
+++ isosplit/up_down.py
@@ -5,22 +5,14 @@ from .jisotonic5 import jisotonic5
 def up_down_isotonic_regression(x, weights=None):
     # determine switch point
     _, mse1 = jisotonic5(x, weights)
-    _, mse2 = jisotonic5(x[::-1].copy(), weights[::-1].copy())
-    mse2 = mse2[::-1]
-    mse0 = mse1 + mse2
+    _, mse2r = jisotonic5(x[::-1].copy(), weights[::-1].copy())
+    mse0 = mse1 + mse2r[::-1]
     best_ind = mse0.argmin()
 
-    # regression
-    if best_ind > 0:
-        y1, _ = jisotonic5(x[:best_ind], weights[:best_ind])
-    if best_ind < x.size:
-        y2, _ = jisotonic5(-x[best_ind:], weights[best_ind:])
-        y2 = -y2
-
-    if best_ind == 0:
-        return y2
-    elif best_ind == x.size:
-        return y1
+    # regressions. note the negatives for decreasing.
+    y1, _ = jisotonic5(x[:best_ind], weights[:best_ind])
+    y2, _ = jisotonic5(-x[best_ind:], weights[best_ind:])
+    y2 = -y2
 
     return np.hstack([y1, y2])
 
